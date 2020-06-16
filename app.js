8ab8a8013453888aca72000adce15d099b5a756b
SaquibAsghar <saq9516@gmail.com>
2020-06-16 23:19:04 +0530

    Created Ajax call

---
+++ app.js
@@ -0,0 +1,58 @@
+document.getElementById("button1").addEventListener("click", loadCustomer);
+document.getElementById("button2").addEventListener("click", loadCustomers);
+
+function loadCustomer(e) {
+	const xml = new XMLHttpRequest();
+
+	xml.open("get", "customer.json", true);
+
+	xml.onload = function () {
+		if (this.status === 200) {
+			const customer = JSON.parse(this.responseText);
+
+			const output = `
+            <ul>
+                <li>Id: ${customer.id} </li>
+                <li>Name: ${customer.name} </li>
+                <li>Company: ${customer.company} </li>
+                <li>Phone: ${customer.phone} </li>
+            
+            </ul>
+            `;
+
+			document.getElementById("customer").innerHTML = output;
+		}
+	};
+
+	xml.send();
+}
+
+function loadCustomers() {
+	const xml = new XMLHttpRequest();
+
+	xml.open("get", "customers.json", true);
+
+	xml.onload = function () {
+		if (this.status === 200) {
+			const customers = JSON.parse(this.responseText);
+
+			let output = "";
+
+			customers.forEach((customer) => {
+				output += `
+            <ul>
+                <li>Id: ${customer.id} </li>
+                <li>Name: ${customer.name} </li>
+                <li>Company: ${customer.company} </li>
+                <li>Phone: ${customer.phone} </li>
+            
+            </ul>
+            `;
+			});
+
+			document.getElementById("customers").innerHTML = output;
+		}
+	};
+
+	xml.send();
+}
